7bc7300bc7ef97cdfc2e18e368bba6cd4f5b42a3
creamfields <88874810+creamfields@users.noreply.github.com>
2021-09-13 19:46:39 +0300

    form, reg_obr, sendMail-php

---
+++ reg_obr.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = $_POST[`name`];
+$lastname = $_POST[`lastname`];
+$email = $_POST[`email`];
+$pass = $_POST[`pass`];
+
+echo "Имя: $name<br>
+Фамилия: $lastname<br>
+Email: $email<br>
+Пароль: $pass";
+?>
